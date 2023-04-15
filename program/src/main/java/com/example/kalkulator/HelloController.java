3c544a6bafd68ddbf9d3f8543406c4fe3ca3a024
jarek <39216951+jarek7410@users.noreply.github.com>
2023-04-15 21:26:08 +0200

    Improved understanding of the concept and made significant code changes for implementation.

--- program/src/main/java/com/example/kalkulator/HelloController.java
+++ program/src/main/java/com/example/kalkulator/HelloController.java
@@ -6,16 +6,20 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
+
+    private int n;
+    private int r;
+
     @FXML
     public Label outputTextOld;
     private String SteteOfOutput;
     private double output=0;
-    private int placesLimit=9;
+    private int placesLimit=10;
     private int places=0;
-    private double bufCurrent=0;
-    private double bufOld=0;
+    private String bufferCurrent="0";
+    private String bufferOld ="0";
     private Operations lastOperation= Operations.none;
-    private boolean dotPressed=false;
+    private boolean isDotPressed =false;
     private int decimalPlacec=0;
     @FXML
     private Label outputText;
@@ -27,24 +31,12 @@ public class HelloController {
         outputText.setText(button.getId());
         String s=button.getId();
         if(s.contains("num")){
-            if(!(places<placesLimit)){
-                return;
-            }else {
-                places++;
-            }
-            int i = Integer.parseInt(String.valueOf(s.charAt(3)));
-            if(dotPressed){
-                decimalPlacec++;
-                bufCurrent += i /Math.pow(10,decimalPlacec);
-
-            }
-            else{
-                bufCurrent *=10;
-                bufCurrent += i;
-            }
+            imputNumber(s);
         }
-        if(s.contains("dot")){
-            dotPressed=true;
+        if(s.contains("dot")
+            &&!bufferCurrent.contains(".")){
+
+            bufferCurrent+=".";
         }
         if(s.equals("add")){
             doLastOperation(Operations.add);
@@ -59,32 +51,61 @@ public class HelloController {
             doLastOperation(Operations.multiply);
         }
         if(s.equals("plsu")){
-            bufCurrent*=-1;
+            //bufferCurrent *=-1;
         }
-        outputTextOld.setText(String.valueOf(bufOld));
-        outputText.setText(String.valueOf(bufCurrent));
+        if(s.equals("sin1")){
+
+        }
+        outputTextOld.setText(String.valueOf(bufferOld));
+        outputText.setText(String.valueOf(bufferCurrent));
+
+    }
+    private void imputNumber(String number){
+        if(!(bufferCurrent.length()<placesLimit)){
+            return;
+        }
+        int i = Integer.parseInt(String.valueOf(number.charAt(3)));
+        if(!isDotPressed){
+
+        }
+        bufferCurrent += i;
 
     }
     private void doLastOperation(Operations newOperation){
+        var numberFirst = Double.valueOf(bufferOld);
+        var numberSecend = Double.valueOf(bufferCurrent);
+        double output=0;
         switch (lastOperation){
             case add -> {
-                bufOld+=bufCurrent;
+                output=numberFirst+numberSecend;
             }
             case subtract -> {
-                bufOld-=bufCurrent;
+                output=numberFirst-numberSecend;
             }
             case none -> {
-                bufOld=bufCurrent;
+                output=numberSecend;
             }
             case multiply -> {
-                bufOld*=bufCurrent;
+                output=numberFirst*numberSecend;
             }
             case divide -> {
-                bufOld/=bufCurrent;
+                output=numberFirst/numberSecend;
             }
         }
         lastOperation=newOperation;
-        bufCurrent=0;
-        System.out.println(bufOld);
+        bufferCurrent ="0";
+        bufferOld=Double.toString(output);
+        refreshOutput();
+    }
+
+    private void refreshOutput() {
+        if(bufferOld.length()>placesLimit){
+            bufferOld=bufferOld.substring(0,placesLimit);
+        }
+        if(bufferCurrent.length()>placesLimit){
+            bufferCurrent=bufferCurrent.substring(0,placesLimit);
+        }
+        outputText.setText(bufferCurrent);
+        outputTextOld.setText(bufferOld);
     }
 }
