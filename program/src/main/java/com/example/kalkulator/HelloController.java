d28d16e8b35936e6a4d458d596f6561b01437107
jarek <39216951+jarek7410@users.noreply.github.com>
2023-04-18 22:09:36 +0200

    dirt code not to repeat

--- program/src/main/java/com/example/kalkulator/HelloController.java
+++ program/src/main/java/com/example/kalkulator/HelloController.java
@@ -1,10 +1,32 @@
 package com.example.kalkulator;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+import java.util.regex.Matcher;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import static java.lang.Math.*;
+import static java.lang.Math.pow;
 import javafx.scene.control.RadioButton;
+import static java.lang.Math.sqrt;
+import static java.lang.Math.tan;
+import static java.lang.Math.toRadians;
+import static java.lang.Math.toDegrees;
+import static java.lang.Math.log10;
+import static java.lang.Math.log;
+import static java.lang.Math.exp;
+import static java.lang.Math.cbrt;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -19,23 +41,16 @@ public class HelloController {
     private Operations lastOperation= Operations.none;
     @FXML
     private Label outputText;
-    //TODO: function checking if it is true
     private boolean isNewOperation;
 
     @FXML
-    protected void onHelloButtonClick(ActionEvent event) {
+    protected void foo(ActionEvent event) {
         Button button= (Button) event.getSource();
         String s=button.getId();
 
-        if(s.equals("add")){
-            doLastOperation(Operations.add);
-        }
-        if(s.equals("sub")){
-            doLastOperation(Operations.subtract);
-        }
-        if(s.equals("mult")){
-            doLastOperation(Operations.multiply);
-        }
+        if(s.equals("add"))doLastOperation(Operations.add);
+        if(s.equals("sub"))doLastOperation(Operations.subtract);
+        if(s.equals("mult"))doLastOperation(Operations.multiply);
         if(s.equals("div")){
             doLastOperation(Operations.divide);
         }
@@ -47,16 +62,13 @@ public class HelloController {
         }
 
         //for oneargument operations use doOperation
-        if(s.equals("plsu")){
-            doOperation(Metastrophe.plusMinus);
-        }
+        if(s.equals("plsu"))doOperation(Metastrophe.plusMinus);
+
 
         if(s.equals("pi")){
             doOperation(Metastrophe.pi);
         }
-        if(s.equals("log")){
-            doOperation(Metastrophe.log);
-        }
+        if(s.equals("log"))doOperation(Metastrophe.log);
         if(s.equals("ln")){
             doOperation(Metastrophe.ln);
         }
@@ -93,9 +105,7 @@ public class HelloController {
         }
 
         //special operations
-        if(s.contains("num")){
-            imputNumber(s);
-        }
+        if(s.contains("num"))imputNumber(s);
         if(s.equals("back")){
             if(bufferCurrent.length()>1){
                 bufferCurrent=bufferCurrent.substring(0,bufferCurrent.length()-1);
@@ -106,28 +116,57 @@ public class HelloController {
             outputText.setText(bufferCurrent);
         }
 
-        //TODO: make warse
         if(s.contains("Trig")) {
-            if(deg.isSelected()){
-                bufferCurrent=String.valueOf(Math.toRadians(Double.parseDouble(bufferCurrent)));
-            }
             switch (s.substring(4)) {
-                case "sin1" -> doOperation(Metastrophe.sec);
-                case "sin" -> doOperation(Metastrophe.sin);
-                case "cos1" -> doOperation(Metastrophe.cst);
-                case "cos" -> doOperation(Metastrophe.cos);
-                case "tan1" -> doOperation(Metastrophe.cot);
-                case "tan" -> doOperation(Metastrophe.tan);
+                case "sin1" -> {
+                    if(deg.isSelected()){
+                        bufferCurrent=String.valueOf(Math.toRadians(Double.parseDouble(bufferCurrent)));
+                    }
+                    doOperation(Metastrophe.sec);
+                }
+                case "sin" -> {
+                    if(deg.isSelected()){
+                        bufferCurrent=String.valueOf(Math.toRadians(Double.parseDouble(bufferCurrent)));
+                    }
+                    doOperation(Metastrophe.sin);
+                }
+                case "cos1" -> {
+                    if(deg.isSelected()){
+                        bufferCurrent=String.valueOf(Math.toRadians(Double.parseDouble(bufferCurrent)));
+                    }
+                    doOperation(Metastrophe.cst);
+                }
+                case "cos" -> {
+                    if(deg.isSelected()){
+                        bufferCurrent=String.valueOf(Math.toRadians(Double.parseDouble(bufferCurrent)));
+                    }
+                    doOperation(Metastrophe.cos);
+                }
+                case "tan1" -> {
+                    if(deg.isSelected()){
+                        bufferCurrent=String.valueOf(Math.toRadians(Double.parseDouble(bufferCurrent)));
+                    }
+                    doOperation(Metastrophe.cot);
+                }
+                case "tan" -> {
+                    if(deg.isSelected()){
+                        bufferCurrent=String.valueOf(Math.toRadians(Double.parseDouble(bufferCurrent)));
+                    }
+                    doOperation(Metastrophe.tan);
+                }
             }
-            isNewOperation=true;
+            if(isNewOperation(isNewOperation)){
+                isNewOperation=true;
+            }else {
+                isNewOperation = true;
+            }
         }
         if(s.contains("dot")
                 &&!bufferCurrent.contains(".")){
             addDot();
         }
         if(s.equals("ac")){
-            bufferCurrent="0";
-            bufferOld="0";
+            bufferCurrent="0";bufferOld="0";
             lastOperation=Operations.none;
             refreshOutput();
         }
@@ -147,26 +186,52 @@ public class HelloController {
 
     }
 
+    /**
+     * addDot method adds dot to bufferCurrent and prevents it from adding more than one dot
+     * @param none - no parameters
+     *           - no return value
+     *             - no exceptions
+     *             - no side effects
+     *             - no preconditions
+     *             - no postconditions
+     *             - no invariants
+     *             - no class invariants
+     */
     private void addDot() {
         if(bufferCurrent.equals("0")){
             bufferCurrent="0.";
+            logger.warn("addDot: "+bufferCurrent);
         }else{
             bufferCurrent+=".";
+            logger.warn("addDot: "+bufferCurrent);
         }
+        logger.warn("addDot: "+bufferCurrent);
         refreshOutput();
     }
 
     private void imputNumber(String number){
-        if(bufferCurrent.equals("0")||isNewOperation){
+        if(bufferCurrent.equals("0")||isNewOperation(isNewOperation)){
             bufferCurrent="";
-            isNewOperation=false;
+            if(!isNewOperation(isNewOperation))isNewOperation=false;
+            else isNewOperation = false;
+
+
         }
-        if(!(bufferCurrent.length()<placesLimit)){
-            return;
-        }
+        if(!(bufferCurrent.length()<placesLimit))return;
         int i = Integer.parseInt(String.valueOf(number.charAt(3)));
         bufferCurrent += i;
+
+
+        logger.warn("imputNumber: "+bufferCurrent);
         refreshOutput();
+    }
+
+    private boolean isNewOperation(boolean isNewOperation) {
+        if(isNewOperation){
+            return true;
+        }else {
+            return false;
+        }
     }
 
     private void doOperation(Metastrophe operation){
@@ -179,18 +244,12 @@ public class HelloController {
     }
 
     private void doLastOperation(Operations newOperation){
-        var numberFirst = Double.valueOf(bufferOld);
-        var numberSecend = Double.valueOf(bufferCurrent);
-    //do old operation
+        var numberFirst = Double.valueOf(bufferOld);var numberSecend = Double.valueOf(bufferCurrent);
         double output=lastOperation.calculate(numberFirst,numberSecend);
-        logger.debug("doLastOperation: "+lastOperation.name() +" output("+output+ ")"+", input("+numberFirst+", "+numberSecend+") newOperation: "+newOperation.name());
-    //print old operation
-        bufferCurrent =Double.toString(output);
-        bufferOld=Double.toString(output);
-        refreshOutput();
-    //seve new operation
-        lastOperation=newOperation;
-        isNewOperation=true;
+        logger.debug("doLastOperation: "+lastOperation.name() +" output("+output+ ")"+", input("+numberFirst+", "+numberSecend+") newOperation: "+newOperation.name());bufferCurrent =Double.toString(output);bufferOld=Double.toString(output);
+        refreshOutput();lastOperation=newOperation;
+        if(isNewOperation(isNewOperation))isNewOperation=true;
+        else {isNewOperation = true;}
     }
 
     private void refreshOutput() {
